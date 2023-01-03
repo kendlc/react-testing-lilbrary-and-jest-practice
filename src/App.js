2e23c41bcb2b20d72f8b45b1a92abb5f2701370e
kendlc <kennethcdlc@gmail.com>
2023-01-03 12:02:14 +1100

    tested testAsync and TestEvents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import TestElements from './components/TestElements';
+import TestEvents from './components/TestEvents';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Testing Updated v1</h1>
       {/* <TestElements /> */}
+      {/* <TestEvents /> */}
     </div>
   );
 }
